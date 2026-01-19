01692308d7e41751a7410f97d7e602b8ff83ca6b
capnerss <capners29@gmail.com>
2026-01-19 04:09:24 +0200

    DIG-10 [Backend] Kirjutada Pythoni funktsioon @eel.expose get_courses(), mis tagastab JSON-i sisu.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import eel
+import json
+import os
+
 
 eel.init('web')
 
@@ -9,4 +12,25 @@ def hello_world():
     return result
 
 
-eel.start('index.html', size=(800, 600), port=5000)
+@eel.expose
+def get_courses():
+    """
+    Read fail data.json and return list of courses.
+    """
+    try:
+        # Open file data.json and read it. encoding='utf-8' is important!
+        with open('data.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            # return list of courses
+            return data
+
+    except FileNotFoundError:
+        print("Error: File not found!")
+        return []
+    except json.JSONDecodeError as e:
+        print(f"Error wrong JSON format: {e}")
+        return []
+
+
+if __name__ == '__main__':
+    eel.start('index.html', size=(1000, 700))
